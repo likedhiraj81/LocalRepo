e2040485cfb72c60dcd08cc6446274a901b3b656
Dhiraj <Dhiraj@Dhiraj-PC>
2022-03-14 09:25:49 +0530

    update config file

--- FunctionalityTestingUsingWithJava/src/com/java/SpringBootRestApplicationTests.java
+++ FunctionalityTestingUsingWithJava/src/com/java/SpringBootRestApplicationTests.java
@@ -14,3 +14,9 @@ public class SpringBootRestApplicationTests {
 	}
 
 }
+spring.datasource.username=HIBERNATE_TEST
+spring.datasource.password=HIBERNATE_TEST
+spring.datasource.driver.class=oracle.jdbc.driver.OracleDriver
+# logging
+logging.pattern.console=%d{yyyy-MM-dd HH:mm:ss} %-5level %logger{36} - %msg%n
+logging.level.org.hibernate.SQL=debug
